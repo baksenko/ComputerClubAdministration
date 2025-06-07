c2ce5b0c93d7b81046117028bc2f8bab2991e165
danil <danilovmv06@gmail.com>
2025-06-07 17:32:38 +0200

    comp repo + equipment repo

---
+++ src/Infrastrucutre/Data/Repositories/ComputerClubRepository.cs
@@ -0,0 +1,29 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastrucutre.Data.Repositories;
+
+public class ComputerClubRepository(ComputerClubDbContext context) : EntityRepository<ComputerClub>(context)
+{
+    public async Task<ComputerClub?> GetComputerclubByAdress(string adress)
+    {
+        var computerclub = _context
+            .Set<ComputerClub>()
+            .AsNoTracking()
+            .Where(x => x.Address == adress)
+            .FirstOrDefaultAsync();
+        
+        return await computerclub;
+    }
+    
+    public async Task<ComputerClub?> GetComputerclubByName(string name)
+    {
+        var computerclub = _context
+            .Set<ComputerClub>()
+            .AsNoTracking()
+            .Where(x => x.Name == name)
+            .FirstOrDefaultAsync();
+        
+        return await computerclub;
+    }
+}
---
+++ src/Infrastrucutre/Data/Repositories/EquipmentRepository.cs
@@ -0,0 +1,29 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastrucutre.Data.Repositories;
+
+public class EquipmentRepository(ComputerClubDbContext context) : EntityRepository<Equipment>(context)
+{
+    public async Task<Equipment?> GetEquipmentBySerialNumberAsync(string serialNumber)
+    {
+        var equipment = _context
+            .Set<Equipment>()
+            .AsNoTracking()
+            .Where(x => x.SerialNumber == serialNumber)
+            .FirstOrDefaultAsync();
+        
+        return await equipment;
+    }
+    
+    public async Task<Equipment?> GetEquipmentByModelAsync(string model)
+    {
+        var equipment = _context
+            .Set<Equipment>()
+            .AsNoTracking()
+            .Where(x => x.Model == model)
+            .FirstOrDefaultAsync();
+        
+        return await equipment;
+    }
+}
